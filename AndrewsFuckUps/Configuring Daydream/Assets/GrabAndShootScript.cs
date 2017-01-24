f6ab0896a67ddf056b44622fbc69f9ad2a8a1af5
Andrew Shea <ashea2nd@mit.edu>
2017-01-23 19:23:53 -0500

    fuck ups

---
+++ AndrewsFuckUps/Configuring Daydream/Assets/GrabAndShootScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabAndShootScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject sword;
+	public GameObject teleport;
+	//private bool swordIsActive = sword.activeInHierarchy;
+
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
